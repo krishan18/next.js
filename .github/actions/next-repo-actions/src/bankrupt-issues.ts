71567adb953bff4291823010924767862240fa9d
samcx <sam@vercel.com>
2024-06-10 15:19:53 -0700

    chore(github-workflow): test bankrupt-issues

--- .github/actions/next-repo-actions/src/bankrupt-issues.ts
+++ .github/actions/next-repo-actions/src/bankrupt-issues.ts
@@ -6,38 +6,34 @@ async function main() {
 
   const octokit = getOctokit(process.env.GITHUB_TOKEN)
   const { owner, repo } = context.repo
-  // const query = `repo:${owner}/${repo} is:issue is:open created:2020-01-01..2020-12-31`
   const commentBody = `
 
-Hello everyone,
+Hi everyone!
 
-Here is the [bug report](https://github.com/vercel/next.js/issues/new?assignees=&labels=bug&projects=&template=1.bug_report.yml).
+We are currently in the process of closing older issues to keep our repository clean and focused. If you believe your issue is still relevant, we encourage you to reopen the same issue using the [bug report template](https://github.com/vercel/next.js/issues/new?assignees=&labels=bug&projects=&template=1.bug_report.yml). If there is context in the original issue, please include it in your new issue.
 
 Thank you for your understanding and contributions!
 
 Best regards,
-The Next.js Team, @samcx
+The Next.js Team
   `
 
-  // let issues: number[] = []
-
   try {
-    // const { data } = await octokit.rest.search.issuesAndPullRequests({
-    //   q: query,
-    // })
-
-    // info(`Total # of issues = ${data.items.length}`)
-
-    // data.items.forEach((issue) => {
-    //   issues.push(issue.number)
-    // })
-
     await octokit.rest.issues.createComment({
       owner,
       repo,
       issue_number: 66573,
       body: commentBody,
     })
+
+    await octokit.rest.issues.addAssignees({
+      owner,
+      repo,
+      issue_number: 66573,
+      assignees: ['samcx'],
+    })
+
+    info(`Commented on and assigned issue #66573 to @samcx`)
   } catch (error) {
     setFailed(error)
   }
